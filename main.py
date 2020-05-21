1a62c996b73aae1f69d7079c80d0833e39deb84f
Pablo-D-P-C <63258562+Pablo-D-P-C@users.noreply.github.com>
2020-05-21 21:09:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,77 @@
-from flask import Flask, request, render_template, make_response
-import random
+from flask import Flask, request, render_template, make_response, redirect, url_for
+from models import User, db
+import random, uuid, hashlib
 
 
 app = Flask(__name__)
+db.create_all()
 
 
 @app.route("/", methods=["GET"])
 def index():
-    secret_number = request.cookies.get("secret_number")
-    response = make_response(render_template("index.html"))
-    if secret_number is None:
-        secret_number = random.randint(0, 30)
-        response.set_cookie("secret_number", str(secret_number))
-    return response
+    session_token = request.cookies.get("session_token")
+
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
+    else:
+        user = None
+
+    return render_template("index.html", user=user)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+    password = request.form.get("user-password")
+
+    secret_number = random.randint(1, 30)
+
+    user = db.query(User).filter_by(email=email).first()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    if not user:
+        user = User(name=name, email=email, secret_number=secret_number, password=hashed_password)
+        db.add(user)
+        db.commit()
+
+    if hashed_password != user.password:
+        return "WRONG PASSWORD! Go back and try again."
+    else:
+        session_token = str(uuid.uuid4())
+        user.session_token = session_token
+        db.add(user)
+        db.commit()
+
+        response = make_response(redirect(url_for("index")))
+        response.set_cookie("session_token", session_token, httponly=True, samesite="Strict")
+        return response
 
 
 @app.route("/result", methods=["POST"])
 def result():
     guess = int(request.form.get("guess"))
-    secret_number = int(request.cookies.get("secret_number"))
 
-    if guess == secret_number:
-        message = "CORRECT! THE SECRET NUMBER IS {0}".format(str(secret_number))
-        response = make_response(render_template("result.html", message=message))
-        response.set_cookie("secret_number", str(random.randint(0, 30)))
-        return response
-    elif guess > secret_number:
+    session_token = request.cookies.get("session_token")
+
+    user = db.query(User).filter_by(session_token=session_token).first()
+
+    if guess == user.secret_number:
+        message = "CONGRATULATIONS!!! THE SECRET NUMBER IS {0}".format(str(guess))
+
+        new_secret = random.randint(1, 30)
+
+        user.secret_number = new_secret
+
+        db.add(user)
+        db.commit()
+
+    elif guess > user.secret_number:
         message = "Your guess is not correct... try something smaller."
-        return render_template("result.html", message=message)
-    elif guess < secret_number:
+    elif guess < user.secret_number:
         message = "Your guess is not correct... try something bigger."
-        return render_template("result.html", message=message)
+
+    return render_template("result.html", message=message)
 
 
 if __name__ == "__main__":
